c0888ef3e54e37c491566c2d7bc6fbdd16f94d95
acarcastillo <33205655+acarcastillo@users.noreply.github.com>
2021-01-25 16:19:07 -0600

    Add files via upload

---
+++ springboot-products-eureka-server/src/main/java/com/products/eureka/SpringbootProductsEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.products.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringbootProductsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProductsEurekaServerApplication.class, args);
+	}
+
+}
